2619ddc5b04f12f6fdc3c486cd205c5dda549d05
Dylan Rooney <dylantrooney1@gmail.com>
2021-04-01 22:42:31 +1100

    script additions

--- js/script.js
+++ js/script.js
@@ -94,3 +94,139 @@ function askQstns() {
     }
 };
 
+function countDownTimer() {
+    var timeInterval = setInterval(function () {
+        timercountremaining--;
+        timerCount.textContent = timercountremaining;
+
+        if (timercountremaining <= 0) {
+            clearInterval(timeInterval);
+            gameOver();
+        }
+    }, 1000);
+};
+
+function gameOver() {
+    timercountremaining = 1;
+
+displayQstn.setAttribute("class", "hideBtn");
+answerBtnEl.setAttribute("class", "hideBtn");
+
+endGame();
+};
+
+function endgame() {
+    var gameOverEl = document.createElement("div");
+    gameOverEl.setAttribute("id", "qstnText");
+    gameOverEl.textContent = "Game over! Please enter your highscore";
+    highscoreInput.appendChild(gameOverEl);
+
+    var showScore = document.createElement("p");
+    showScore.setAttribute("class", "highscoreInput");
+    showScore.textContent = "Your score is: " + highscore;
+    highscoreInput.appendChild(showScore);
+
+    var nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("class", "highscoreInput");
+    nameInput.setAttribute("placeholder", "Enter Name here"); 
+    highscoreInput.appendChild(nameInput);
+
+    var submitBtn = document.createElement("button");
+    submitBtn.setAttribute("class", "playBtn");
+    submitBtn.textContent = "Submit";
+    highscoreInput.appendChild(submitBtn);
+
+    submitBtn.addEventListener("click", function (event) {
+        var userScore = {
+            "name": nameInput.value,
+            "score": highscore
+        };
+
+        var existingScore = JSON.parse(localStorage.getItem("highscore"));
+        if (!existingScore) {
+            existingScore = []
+        };
+
+        existingScore.push(newScore);
+        localStorage.setItem("highscore". JSON.stringify(existingScore));
+
+        if (nameInput.value == "") {
+            console.log("No Value");
+            window.alert("Please enter a valid name");
+            event.preventDefault();
+        } else {
+            seeHighscore();
+        }
+
+        function Highscores() {
+            highscoreInput.innerHTML = "";
+            for (var j = 1; j <= existingScore.length -1; j++) {
+                var seeAllScores = document.createElement("ul");
+                seeAllScores.setAttribute("class", "currentScores");
+                seeAllScores.textContent = "name - " + existingScore[j].name + "Score - " + existingScore[j].score;
+                highscoreInput.appendChild(seeAllScores);
+            }
+
+            var playAgain = document.createElement("button");
+            playAgain.setAttribute("class", "navBtn");
+            playAgain.textContent = "Play Again";
+            highscoreInput.appendChild(playAgain);
+            var clearScores = document.createElement("button");
+            clearScores .setAttribute("class", "navBtn clearScores");
+            clearScores.textContent = "Clear Scores";
+            highscoreInput.appendChild(clearScores);
+
+            clearScores.addEventListener("click", function () {
+                existingScore = [];
+                window.localStorage.clear();
+                seeHighscore();
+                console.log(window.localStorage.highscore);
+            });
+        }
+    });
+}
+
+seeHighscore.addEventListener("click", function(event) {
+     var scoreExisting = JSON.parse(localStorage.getItem("Highscore"));
+    timercountremaining = 1;
+    playBtn.setAttribute("class", "hideBtn");
+    answerBtnEl.innerHTML = "";
+    question.innerHTML = "";
+    highscoreInput.innerHTML = "";
+
+    if (!scoreExisting) {
+        window.alert("No scores available");
+    } else {
+        for (var l = 0; l <= existingScore.length -1; l++) {
+            var seeAllScores = document.createElement("ul");
+            seeAllScores.setAttribute("class", "currentScore");
+            seeAllScores.textContent = "name - " + existingScore[l].name + "Score - " + existingScore[l].score;
+            highscoreInput.appendChild(seeAllScores);
+            event.preventDefault();
+        }
+    };
+
+    var playAgain = document.createElement("button");
+    playAgain.setAttribute("class", "navBtn");
+    playAgain.textContent = "Play Again";
+    highscoreInput.appendChild(playAgain);
+
+    var clearScores = document.createElement("button");
+    clearScores.setAttribute("class", "navBtn clearScore");
+    clearScores.textContent = "Clear Scores";
+    highscoreInput.appendChild(clearScores);
+
+    clearScores.addEventListener("click", function () {
+        existingScore = [];
+        window.localStorage.clear();
+        seeAllScores();
+        console.log(window.localStorage.highscore);
+    });
+});
+
+playBtn.addEventListener("click", function() {
+
+    playGame();
+    playBtn.setAttribute("class", "hideBtn");
+});
